be42f78fe5e056f8a1cb999c06388366c6753194
Jonathan <jono-rogers@hotmail.com>
2019-10-22 10:35:25 +1300

    database set up to populate db

--- application.py
+++ application.py
@@ -1,2 +1,14 @@
 # Catalog project for Udacity
+
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Sport, SportEquipment
+
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///sportcatalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
