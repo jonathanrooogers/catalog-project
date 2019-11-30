43eaa71012db7ec26c48beef7f9bb3ee3760dd12
Jonathan Rogers <jono-rogers@hotmail.com>
2019-12-01 12:38:42 +1300

    connection update

--- populateCatalog.py
+++ populateCatalog.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from database_setup import Base, User, Catagory, Item
 
-engine = create_engine('sqlite:///catalog.db')
+engine = create_engine('postgresql://catalog:yourpassword@localhost/catalog')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
